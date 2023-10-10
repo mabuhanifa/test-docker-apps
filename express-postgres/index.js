deca5148eab106ca596810aee0ccf5055cbec775
mabuhanifa <moh.abuhanifa@gmail.com>
2023-10-10 18:23:34 +0600

    api running

---
+++ express-postgres/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
